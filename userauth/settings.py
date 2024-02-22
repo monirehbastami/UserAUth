8bc1374a85f7bbafb132baa5cd248177c0bffb7e
Monireh Bastami <monirehbastami@gmail.com>
2024-02-22 11:34:19 -0800

    set Swagger

--- userauth/settings.py
+++ userauth/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',
     'user',
+    'schools'
 ]
 
 MIDDLEWARE = [
