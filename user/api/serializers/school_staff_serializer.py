459e35df5923440d620acd76f62e154595e530ff
Monireh Bastami <monirehbastami@gmail.com>
2024-02-23 05:23:53 -0800

    modify create function in serializer

--- user/api/serializers/school_staff_serializer.py
+++ user/api/serializers/school_staff_serializer.py
@@ -8,14 +8,15 @@ class SchoolStaffProfileSerializer(serializers.ModelSerializer):
 
 
 class SchoolStaffSerializer(serializers.ModelSerializer):
-    
+    tprofile = SchoolStaffProfileSerializer(source='school_staff')
     class Meta:
         model = SchoolStaff
         fields = [
             'username',
             'email',
             'password',
-            'phone_number'
+            'phone_number',
+            'tprofile'
         ]
         extra_kwargs = {
             'password': {'write_only': True},
@@ -23,11 +24,40 @@ class SchoolStaffSerializer(serializers.ModelSerializer):
         }
 
     def create(self, validated_data):
-        password = validated_data.pop('password')
-
+        password = validated_data.pop('password', None)
+        t = validated_data.pop('school_staff', None)
         school_staff = super().create(validated_data)
-        school_staff.set_password(password)
-        school_staff.save()
 
+        school_staff_profile = SchoolStaffProfile.objects.get(user=school_staff)
+        if school_staff_profile:
+            school_staff_profile.school = t.get('school')
+            school_staff_profile.save()
+        else:
+            SchoolStaffProfile.objects.create(user=school_staff,school=t.get('school'))
+
+        if password:
+            school_staff.set_password(password)
+            school_staff.save()
         return school_staff
-    
+
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        t = validated_data.pop('school_staff', None)
+
+        # instance.username = validated_data.pop('username', None)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        
+        instance.save()
+
+        school_staff_profile, created = SchoolStaffProfile.objects.get_or_create(user=instance)
+        school_staff_profile.school = t.get('school')
+        school_staff_profile.save()
+
+        if password:
+            instance.set_password(password)
+            instance.save()
+
+        return instance
